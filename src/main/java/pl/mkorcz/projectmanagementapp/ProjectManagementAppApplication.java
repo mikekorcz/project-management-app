31c0c9c4dc02b32ea14962af34ab9bc7d77bf9c0
Michal <mkorcz01@gmail.com>
2021-05-11 21:17:04 +0200

    first commit

---
+++ src/main/java/pl/mkorcz/projectmanagementapp/ProjectManagementAppApplication.java
@@ -0,0 +1,23 @@
+package pl.mkorcz.projectmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+@SpringBootApplication
+public class ProjectManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementAppApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello world";
+	}
+
+}
---
+++ src/test/java/pl/mkorcz/projectmanagementapp/ProjectManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mkorcz.projectmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
