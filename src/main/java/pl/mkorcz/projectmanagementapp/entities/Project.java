96e17dc4d6777eb1e55ce85564c5cba3717920ef
Michal <mkorcz01@gmail.com>
2021-06-16 21:56:40 +0200

    set configuration for postgresql

--- src/main/java/pl/mkorcz/projectmanagementapp/entities/Project.java
+++ src/main/java/pl/mkorcz/projectmanagementapp/entities/Project.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Project {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "project_seq")
     private long projectId;
 
     private String name;
