0d071cd3c624a9e223fad281c0be7aafaeff2130
Michal <mkorcz01@gmail.com>
2021-08-10 20:58:12 +0200

    added value version to configurationFile

--- src/main/java/pl/mkorcz/projectmanagementapp/controllers/HomeController.java
+++ src/main/java/pl/mkorcz/projectmanagementapp/controllers/HomeController.java
@@ -3,6 +3,7 @@ package pl.mkorcz.projectmanagementapp.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,9 @@ import java.util.Map;
 @Controller
 public class HomeController {
 
+    @Value("${version}")
+    private String ver;
+
     @Autowired
     ProjectRepository projectRepo;
 
@@ -27,6 +31,8 @@ public class HomeController {
 
     @GetMapping("/")
     public String displayHome(Model model) throws JsonProcessingException {
+
+        model.addAttribute("versionNumber", ver);
 
         Map<String, Object> map = new HashMap<>();
 
